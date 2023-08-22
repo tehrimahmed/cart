e988e0b735ab66cc656fccc9848350dd845a014b
Ghias Ali <76915661+GhiasAli17@users.noreply.github.com>
2023-08-22 15:39:23 +0500

    single quote changed to double

--- src/redux/PrivateRoute.js
+++ src/redux/PrivateRoute.js
@@ -3,13 +3,13 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 
 const isAuthenticated = () => {
-    const password = localStorage.getItem('password'); 
-    return password == 'password'; 
+    const password = localStorage.getItem("password"); 
+    return password === "password"; 
   };
   
 const isAdmin = () => {
-    const role = localStorage.getItem('role'); 
-    return role === 'admin'; 
+    const role = localStorage.getItem("role"); 
+    return role === "admin"; 
   };
   
 
